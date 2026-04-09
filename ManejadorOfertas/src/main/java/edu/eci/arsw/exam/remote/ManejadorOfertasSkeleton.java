0b9e7fc24fb563cba07ec45474be2b0416688b03
unknown <christian.romero-m@labinfo.is.escuelaing.edu.co>
2026-04-09 17:43:59 -0500

    parcial2

--- ManejadorOfertas/src/main/java/edu/eci/arsw/exam/remote/ManejadorOfertasSkeleton.java
+++ ManejadorOfertas/src/main/java/edu/eci/arsw/exam/remote/ManejadorOfertasSkeleton.java
@@ -6,6 +6,9 @@
 package edu.eci.arsw.exam.remote;
 
 import edu.eci.arsw.exam.FachadaPersistenciaOfertas;
+import edu.eci.arsw.exam.Product;
+import edu.eci.arsw.exam.events.OffertMessageProducer;
+import edu.eci.arsw.exam.events.SaleNotification;
 
 /**
  *
@@ -14,33 +17,65 @@ import edu.eci.arsw.exam.FachadaPersistenciaOfertas;
 public class ManejadorOfertasSkeleton implements ManejadorOfertasStub{
 
     private FachadaPersistenciaOfertas fpers=null;
+    private OffertMessageProducer messageProducer;
+    private WinnerAnnouncer winnerAnnouncer;
 
     public void setFachadaPersistenciaOfertas(FachadaPersistenciaOfertas fpers) {
         this.fpers = fpers;
     }
+
+    public void setMessageProducer(OffertMessageProducer messageProducer) {
+        this.messageProducer = messageProducer;
+    }
+
+    public void setWinnerAnnouncer(WinnerAnnouncer winnerAnnouncer) {
+        this.winnerAnnouncer = winnerAnnouncer;
+    }
             
     @Override
     public void agregarOferta(String codOferente,String codprod,int monto) {
-        
-        if (!fpers.getMapaOfertasRecibidas().containsKey(codprod)){
-            //se ha recibido la primera oferta 
-            fpers.getMapaOfertasRecibidas().put(codprod, 1);
-            //se asigna el monto propuesto como mejor oferta
-            fpers.getMapaMontosAsignados().put(codprod, monto);
-            //se asigna al oferente como ganador provisional
-            fpers.getMapaOferentesAsignados().put(codprod, codOferente);
-        }
-        else{
-            int ofertasActuales=fpers.getMapaOfertasRecibidas().get(codprod);
-            fpers.getMapaOfertasRecibidas().put(codprod,ofertasActuales+1);
-            if (fpers.getMapaMontosAsignados().get(codprod)>monto){
+        Object lock = fpers.getBloqueoProducto(codprod);
+        synchronized (lock) {
+            Integer ofertasRecibidas = fpers.getMapaOfertasRecibidas().get(codprod);
+            if (ofertasRecibidas != null && ofertasRecibidas >= 3) {
+                return;
+            }
+
+            Product producto = fpers.getMapaProductosSolicitados().get(codprod);
+            if (producto == null) {
+                return;
+            }
+
+            if (monto < producto.setStartPrice()) {
+                return;
+            }
+
+            int nuevasOfertas = (ofertasRecibidas == null ? 0 : ofertasRecibidas) + 1;
+            fpers.getMapaOfertasRecibidas().put(codprod, nuevasOfertas);
+
+            Integer mejorMontoActual = fpers.getMapaMontosAsignados().get(codprod);
+            if (mejorMontoActual == null || monto > mejorMontoActual) {
                 fpers.getMapaMontosAsignados().put(codprod, monto);
                 fpers.getMapaOferentesAsignados().put(codprod, codOferente);
             }
+
+            if (nuevasOfertas == 3) {
+                String ganador = fpers.getMapaOferentesAsignados().get(codprod);
+                Integer montoGanador = fpers.getMapaMontosAsignados().get(codprod);
+
+                if (winnerAnnouncer != null) {
+                    winnerAnnouncer.announceWinner(codprod, ganador, montoGanador);
+                }
+
+                if (messageProducer != null) {
+                    messageProducer.sendMessages("my.sale." + ganador, new SaleNotification(ganador, codprod));
+                }
+            }
         }
-        
     }
-    
-    
-    
+
+    public static interface WinnerAnnouncer {
+        void announceWinner(String codProducto, String codComprador, Integer monto);
+    }
+
 }
